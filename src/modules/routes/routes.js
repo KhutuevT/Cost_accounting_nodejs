321b10ee2b79615e28ba49afe7f5361fef590e31
Tamerlan Khutuev <t.khutuev@gmail.com>
2021-12-15 14:46:27 +0300

    add deleteReceipt

--- src/modules/routes/routes.js
+++ src/modules/routes/routes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
   addNewReceipt,
   getAllReceipts,
+  deleteReceipt,
 } = require("../controllers/receipt.controllers");
 
 router.get("/allReceipts", getAllReceipts);
 router.post("/newReceipt", addNewReceipt);
+router.delete("/deleteReceipt", deleteReceipt);
 
 module.exports = router;
